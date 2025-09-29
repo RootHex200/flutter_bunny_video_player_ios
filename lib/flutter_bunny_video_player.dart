170c658a9685335128122f8d4b2bb393f8c2ee0b
Roothex200 <sabiturrahman60@gmail.com>
2025-09-29 22:28:15 +0600

    Update BunnyPlayerView: Modify videoId and libraryId, and add token and expires parameters for enhanced functionality

--- lib/flutter_bunny_video_player.dart
+++ lib/flutter_bunny_video_player.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:io';
 
 import 'package:flutter/services.dart';
@@ -10,6 +8,8 @@ class BunnyPlayerView extends StatefulWidget {
   final String videoId;
   final int libraryId;
   final String? playIconAsset;
+  final String? token;
+  final int? expires;
 
   const BunnyPlayerView({
     super.key,
@@ -17,6 +17,8 @@ class BunnyPlayerView extends StatefulWidget {
     required this.videoId,
     required this.libraryId,
     this.playIconAsset,
+    this.token,
+    this.expires
   });
 
   @override
@@ -24,7 +26,6 @@ class BunnyPlayerView extends StatefulWidget {
 }
 
 class _BunnyPlayerViewState extends State<BunnyPlayerView> {
-
   @override
   void initState() {
 // SystemChrome.setPreferredOrientations([
@@ -40,20 +41,21 @@ class _BunnyPlayerViewState extends State<BunnyPlayerView> {
   Widget build(BuildContext context) {
     const viewType = 'bunny_player_view';
 
-    if(Platform.isIOS){
-    return UiKitView(
-      viewType: viewType,
-      layoutDirection: TextDirection.ltr,
-      creationParams: {
-        'accessKey': widget.accessKey,
-        'videoId': widget.videoId,
-        'libraryId': widget.libraryId,
-        'playIconAsset': widget.playIconAsset,
-      },
-      creationParamsCodec: const StandardMessageCodec(),
-    );
+    if (Platform.isIOS) {
+      return UiKitView(
+        viewType: viewType,
+        layoutDirection: TextDirection.ltr,
+        creationParams: {
+          'accessKey': widget.accessKey,
+          'videoId': widget.videoId,
+          'libraryId': widget.libraryId,
+          'playIconAsset': widget.playIconAsset,
+          'token':widget.token,
+          'expires':widget.expires,
+        },
+        creationParamsCodec: const StandardMessageCodec(),
+      );
     }
     return Container();
   }
 }
-
