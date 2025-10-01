50e08653bbbc3d311fe13b35668212837c601496
Roothex200 <sabiturrahman60@gmail.com>
2025-10-01 21:54:03 +0600

    rename the files name

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bunny_video_player_ios/flutter_bunny_video_player.dart';
+import 'package:flutter_bunny_video_player_ios/flutter_bunny_video_player_ios.dart';
 
 void main() {
   runApp(const MyApp());
